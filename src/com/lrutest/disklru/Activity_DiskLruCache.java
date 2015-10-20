0e3c9b9c5f9840f03a323f628abf0071708fc2d1
FightingHorse <wangzhibin1220@163.com>
2015-10-20 14:58:24 +0800

    更换图片源,添加部分注释

--- src/com/lrutest/disklru/Activity_DiskLruCache.java
+++ src/com/lrutest/disklru/Activity_DiskLruCache.java
@@ -20,25 +20,41 @@ public class Activity_DiskLruCache extends Activity {
 	 */
 	private PhotoWallAdapter mAdapter;
 
+	/**
+	 * 每一张图片的宽度
+	 */
 	private int mImageThumbSize;
+	/**
+	 * 两张图片之间的间隙
+	 */
 	private int mImageThumbSpacing;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_photo_disklru);
+
 		mImageThumbSize = getResources().getDimensionPixelSize(R.dimen.image_thumbnail_size);
 		mImageThumbSpacing = getResources().getDimensionPixelSize(R.dimen.image_thumbnail_spacing);
+
 		mPhotoWall = (GridView) findViewById(R.id.photo_wall);
 		mAdapter = new PhotoWallAdapter(this, 0, Images.imageUrls, mPhotoWall);
 		mPhotoWall.setAdapter(mAdapter);
+
 		mPhotoWall.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
 
 			@Override
 			public void onGlobalLayout() {
+				/**
+				 * 照片墙的列数
+				 */
 				final int numColumns = (int) Math.floor(mPhotoWall.getWidth() / (mImageThumbSize + mImageThumbSpacing));
 				if (numColumns > 0) {
+					/**
+					 * 每一列的宽度
+					 */
 					int columnWidth = (mPhotoWall.getWidth() / numColumns) - mImageThumbSpacing;
+					// 长宽一样
 					mAdapter.setItemHeight(columnWidth);
 					mPhotoWall.getViewTreeObserver().removeGlobalOnLayoutListener(this);
 				}
